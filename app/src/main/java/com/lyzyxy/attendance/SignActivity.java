b7a6b302f54a207007da89d62defec3157f5572a
t0903 <1051836830@qq.com>
2019-06-24 14:15:12 +0800

    扫脸签到

--- app/src/main/java/com/lyzyxy/attendance/SignActivity.java
+++ app/src/main/java/com/lyzyxy/attendance/SignActivity.java
@@ -2,12 +2,50 @@ package com.lyzyxy.attendance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
-public class SignActivity extends AppCompatActivity {
+import com.lyzyxy.attendance.camera.CameraPreview;
+
+public class SignActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
+
+        Button buttonStartPreview = findViewById(R.id.button_start_preview);
+        buttonStartPreview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPreview();
+            }
+        });
+        Button buttonStopPreview = findViewById(R.id.button_stop_preview);
+        buttonStopPreview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopPreview();
+            }
+        });
     }
+
+    public void startPreview() {
+        CameraPreview mPreview = new CameraPreview(this);
+        FrameLayout preview = findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+    }
+
+    public void stopPreview() {
+        FrameLayout preview = findViewById(R.id.camera_preview);
+        preview.removeAllViews();
+    }
+
+    public void onPause() {
+        finish();
+        super.onPause();
+    }
+
+
 }
